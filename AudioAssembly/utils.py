5f2ee59cd71cf17d8700f0fcfc643df2f0f3ec00
musaasad2020 <musaasad.2020@gmail.com>
2022-05-15 11:44:32 -0400

    Update utils.py

--- AudioAssembly/utils.py
+++ AudioAssembly/utils.py
@@ -72,12 +72,11 @@ def interpret_data(data):
         language="German"
     elif language=="it":
         language="Italian"
-    keywords=None
+    keywords=[]
     if data["auto_highlights_result"]:
         keywords=data["auto_highlights_result"]["results"]
-    tone=None
+    tone={"POSITIVE": 0, "NEGATIVE": 0, "NEUTRAL": 0}
     if data["sentiment_analysis_results"]:
-        tone={"POSITIVE": 0, "NEGATIVE": 0, "NEUTRAL": 0}
         for result in data["sentiment_analysis_results"]:
             tone[result["sentiment"]]+=1
         tone["POSITIVE"] = str((tone["POSITIVE"]/len(data["sentiment_analysis_results"]))*100)+"%"
