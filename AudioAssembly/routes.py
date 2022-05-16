fb127bd153b9441316a1bf53adeff1884f0e83a0
musaasad2020 <musaasad.2020@gmail.com>
2022-05-16 14:44:15 -0400

    Update routes.py

--- AudioAssembly/routes.py
+++ AudioAssembly/routes.py
@@ -12,4 +12,14 @@ def report():
     if upload_url:
         return render_template('report.html', data=interpret_data(get_transcript(generate_transcript(upload_url))))
     else:
-        return render_template('report.html', data="")
+        return render_template('report.html', data={
+            "confidence": "90%", 
+            "speakers": 1, 
+            "disfluencies_count": 0, 
+            "language": "English", 
+            "profanity": 0, 
+            "keywords": [{"text": "a"}, {"text": "a"}, {"text": "a"}], 
+            "content_safety": "10%", 
+            "tone": {"POSITIVE": "33.33%", "NEGATIVE": "33.33%", "NEUTRAL": "33.33%"}, 
+            "important_entities": [{"entity_type": "event", "text": "super"}] 
+        })
