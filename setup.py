23696ada9f92ccf43c810105dc39ba4b3e9fcd8b
musaasad2020 <musaasad.2020@gmail.com>
2022-05-20 16:10:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='flask_templates',
-    packages=['flask_templates'],
+    name='AudioAssembly',
+    packages=['AudioAssembly'],
     include_package_data=True,
     install_requires=[
         'flask',
